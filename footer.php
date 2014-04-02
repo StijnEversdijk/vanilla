4cfe31efbce1c76cbbe4b0c8e25bdb3046289088
Stijn Eversdijk <mail@stijneversdijk.nl>
2014-04-02 20:00:41 +0200

    updated footer

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 		<script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.10.2.min.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
 
+    <?php wp_footer(); ?>
+
   </body>
 
 </html>
